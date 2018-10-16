6749747b5e2b585177f2aaf82b5ff806441843c3
Arunkumar <arunkumar@Arunkumars-MacBook-Pro.local>
2018-10-16 13:35:01 +0530

    mlab config and get the latest events

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var bodyParser = require('body-parser');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var cors = require('cors');
+var mongoose = require('mongoose');
+var microsoftGraph = require("@microsoft/microsoft-graph-client");
+
 
 app.use(cors());
 app.use(bodyParser.json()); // support json encoded bodies
@@ -13,15 +16,39 @@ app.use(bodyParser.urlencoded({
 	extended: true
 })); // support encoded bodies
 
-io.on('connection', function(client) {  
-    client.on('join', function(data) {
-		console.log('----------------------------------------------------------------------------------------------------------------------------------');
-        console.log(data);
-    });
-    client.on('messages', function(data) {
-           client.emit('broad', data);
-           client.broadcast.emit('broad',data);
-    });
+const Schema = mongoose.Schema;
+const USER = mongoose.model('user', new Schema({
+	id: mongoose.Schema.Types.ObjectId,
+	user_id: String,
+	token: String,
+}));
+
+const COMPOSE_URI_DEFAULT = 'mongodb://arunkumar:arun123@ds233323.mlab.com:33323/user';
+const connectionOptions = {
+	server: {
+		socketOptions: {
+			socketTimeoutMS: 0,
+			connectionTimeout: 0
+		}
+	}
+};
+
+mongoose.connect(COMPOSE_URI_DEFAULT, connectionOptions, function (error) {
+	if (error) console.error(error)
+	else console.log('mongo connected')
+})
+const conn = mongoose.connection;
+conn.on('error', console.error.bind(console, 'connection error:'));
+
+
+io.on('connection', function (client) {
+	client.on('join', function (data) {
+		console.log(data);
+	});
+	client.on('messages', function (data) {
+		client.emit('broad', data);
+		client.broadcast.emit('broad', data);
+	});
 });
 
 
@@ -38,11 +65,37 @@ app.post('/V1/feed', function (req, res) {
 });
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname+`/index.html`);
+	res.sendFile(__dirname + `/index.html`);
 });
 
-
+app.get('/V1/getOutlookfeed', async (req, res) => {
+	return getGraphClient(req.query.token)
+		.api('/me')
+		.get((err, data) => {
+			let user = new USER({
+				user_id: data.id,
+				token: req.query.token,
+			});
+			user.save();
+			getGraphClient(token)
+				.api('/me/events?$select=subject,body,bodyPreview,organizer,attendees,start,end,location')
+				.get((err, events) => {
+					events.userId = data.id;
+					res.send(events);
+				});
+		});
+});
 
 // start the server
 http.listen(port);
 console.log('Server started! At http://localhost:' + port);
+
+function getGraphClient(token) {
+	return microsoftGraph.Client.init({
+		defaultVersion: 'v1.0',
+		debugLogging: true,
+		authProvider: (done) => {
+			done(null, token);
+		}
+	});
+}
